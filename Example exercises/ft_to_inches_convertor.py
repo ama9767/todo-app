4332a1c13c64b156c3c3a153ca9d87d2a96fac00
ama9767 <annageorgiou9@outlook.com>
2025-06-13 16:19:54 +0300

    Added exit button to ft_to_inches_convertor

--- Example exercises/ft_to_inches_convertor.py
+++ Example exercises/ft_to_inches_convertor.py
@@ -1,6 +1,7 @@
 import FreeSimpleGUI as sg
 from ft_inch_function import convert
 
+sg.theme("Topanga")
 
 label_ft = sg.Text("Enter feet:")
 input_ft = sg.Input(key="feet")
@@ -9,15 +10,22 @@ label_inch = sg.Text("Enter inches:")
 input_inch = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 output_result = sg.Text("", key="output")
 
 window = sg.Window("Convert in to m",
                    layout= [[label_ft, input_ft],
                             [label_inch, input_inch],
-                            [convert_button, output_result]])
+                            [convert_button, exit_button, output_result]])
 
 while True:
     event, values = window.read()
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
+
     feet = float(values["feet"])
     inches = float(values["inches"])
 
