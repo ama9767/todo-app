a122350e6e230aee5cde3deff8507538995f89ad
ama9767 <annageorgiou9@outlook.com>
2025-06-14 13:14:20 +0300

    Updated function add_todo to clear input box, resolved label empty value warning & added clear all functionality with user confirmation

--- web.py
+++ web.py
@@ -4,21 +4,38 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
-
+    todo_local = st.session_state["new_todo"].strip()
+    if todo_local:  # only add if not empty
+        todos.append(todo_local)
+        functions.write_todos(todos)
+        st.session_state["new_todo"] = ""  # clear input box
 
 st.title("My Todo App")
-st.subheader("Python web app")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+# Display todos with checkboxes
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="", placeholder="Add new todo..",
+# Input box for new item
+st.text_input(label="New Todo",
+              label_visibility="collapsed",
+              placeholder= "Add new todo..",
               on_change=add_todo, key='new_todo')
 
-
-# Remove after publish
-st.session_state
+# Clear All button with confirmation
+with st.expander("Clear All Todos"):
+    st.warning("This will delete all your todos permanently!")
+    if st.button("Yes, clear all"):
+        todos.clear()
+        functions.write_todos(todos)
+        # Reset session_state keys for checkboxes
+        for key in list(st.session_state.keys()):
+            if key != 'new_todo':
+                del st.session_state[key]
+                st.rerun()
