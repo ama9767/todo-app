0ac3fa66ec20e6d5c32d3a55c0e9f30751d4db3c
ama9767 <annageorgiou9@outlook.com>
2025-06-14 12:40:45 +0300

    Added a function for new todo items

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("Python web app")
 st.write("This app is to increase your productivity.")
@@ -10,6 +16,9 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="", placeholder="Add new todo..")
+st.text_input(label="", placeholder="Add new todo..",
+              on_change=add_todo, key='new_todo')
 
 
+# Remove after publish
+st.session_state
